8e995cdb3128eff64ef6fea7a6e7c14c91267f20
Paulo Gouvea <paulogouvea1994@gmail.com>
2021-10-06 21:11:23 -0300

    Componente de Botão

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Input } from '../../components/Forms/Input';
+import { Button } from '../../components/Forms/Button';
 
 import {
     Container,
     Header,
     Title,
-    Form
+    Form,
+    Fields,
 } from './styles'
 
 export function Register() {
@@ -16,12 +18,18 @@ export function Register() {
             </Header>
 
             <Form>
-                <Input 
-                    placeholder="Nome"
-                />
+                <Fields>
+                    <Input 
+                        placeholder="Nome"
+                    />
 
-                <Input 
-                    placeholder="Preço"
+                    <Input 
+                        placeholder="Preço"
+                    />
+                </Fields>
+
+                <Button 
+                    title="Enviar"
                 />
             </Form>
         </Container>
