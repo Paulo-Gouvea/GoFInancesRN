3d209663e89eee9372e03b8c7a32675fbb818f58
Paulo-Gouvea <paulogouvea1994@gmail.com>
2021-10-26 11:55:59 -0300

    Criando autenticação Apple

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -7,6 +7,10 @@ import React, {
 
 import * as AuthSession from 'expo-auth-session';
 
+import * as AppleAuthentication from 'expo-apple-authentication';
+
+import AsyncStorage from "@react-native-async-storage/async-storage"; 
+
 interface AuthProviderProps {
     children: ReactNode;
 }
@@ -21,6 +25,7 @@ interface User {
 interface AuthContextData {
     user: User;
     signInWithGoogle(): Promise<void>;
+    signInWithApple(): Promise<void>;
  } 
 
  interface AuthorizationResponse {
@@ -68,10 +73,35 @@ function AuthProvider({ children }: AuthProviderProps){
         }
     } 
 
+    async function signInWithApple(){
+        try {
+            const credential = await AppleAuthentication.signInAsync({
+                requestedScopes: [
+                    AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+                    AppleAuthentication.AppleAuthenticationScope.EMAIL,
+                ]
+            });
+
+            if(credential){
+                const userLogged = {
+                    id: String(credential.user),
+                    email: credential.email!,
+                    name: credential.fullName!.givenName!,
+                    photo: undefined
+                }
+
+                setUser(userLogged);
+                await AsyncStorage.setItem('@gofinances:user', JSON.stringify(userLogged));
+            }
+        } catch (error: any | unknown) {
+            throw new Error(error);
+        }
+    }
+
     return (
         <AuthContext.Provider 
             value={{
-                user, signInWithGoogle
+                user, signInWithGoogle, signInWithApple
             }}
         >
             { children }
